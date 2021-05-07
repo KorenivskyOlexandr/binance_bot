6de5cf669023fd88e302ef18deae6a81c7d7da22
KorenivskyOlexandr <korenivsky.alexandr@gmail.com>
2021-05-07 18:57:46 +0300

    add telegram bot notification

--- binance_bot.py
+++ binance_bot.py
@@ -1,8 +1,8 @@
 from telebot import TeleBot
-from constant import bot_token
+from constant import bot_token, my_chat_id
 
 bot = TeleBot(bot_token)
 
 
 def send_notification_message(text: str) -> None:
-    bot.send_message(342974404, text)
+    bot.send_message(my_chat_id, text)
